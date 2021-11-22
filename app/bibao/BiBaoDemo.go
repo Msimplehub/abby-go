b5458de0752ede7dafeb066e0b64135db2b2a7d1
sunjunhong <~Hello123>
2021-11-22 12:04:43 +0800

    闭包

---
+++ app/bibao/BiBaoDemo.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	a()
+	c := a()
+	c()
+	c()
+	c()
+
+	a()
+}
+
+func a() func() int {
+
+	i := 0
+	b := func() int {
+		i++
+		fmt.Println(i)
+		return i
+	}
+
+	return b
+}
